bd32f662c36b17d891af0f1ccfb4e06ca59f2273
Lavender <susie204@naver.com>
2024-06-22 20:57:30 +0900

    모션 만들기 화면 틀 작성

---
+++ EpsonChillin/MotionResult/MotionResultController.swift
@@ -0,0 +1,8 @@
+//
+//  MotionResultController.swift
+//  EpsonChillin
+//
+//  Created by 이승현 on 6/22/24.
+//
+
+import Foundation
---
+++ EpsonChillin/MotionResult/MotionResultView.swift
@@ -0,0 +1,14 @@
+//
+//  MotionView.swift
+//  EpsonChillin
+//
+//  Created by 이승현 on 6/22/24.
+//
+
+import UIKit
+import SnapKit
+
+class MotionView: BaseView {
+    
+    
+}
