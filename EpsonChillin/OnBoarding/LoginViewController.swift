a669e0bae7a513152e5ba36b21bb645a02068a4f
Lavender <susie204@naver.com>
2024-09-26 00:43:18 +0900

    애플로그인 UI

--- EpsonChillin/OnBoarding/LoginViewController.swift
+++ EpsonChillin/OnBoarding/LoginViewController.swift
@@ -6,24 +6,62 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loginView.appleLoginButton.addTarget(self, action: #selector(appleLoginButtonTapped), for: .touchUpInside)
+    }
+    
+    override func loadView() {
+        self.view = loginView
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func appleLoginButtonTapped() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self //로직
+        controller.presentationContextProvider = self //로그인 창
+        controller.performRequests()
     }
-    */
 
 }
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window! //윈도우에 꽉 차게
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    
+    //애플 로그인이 성공한 경우 -> 메인 페이지로 이동 등..
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            print(userIdentifier)
+            print(fullName)
+            print(email)
+        }
+    }
+    
+    //애플로 로그인 실패한 경우
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("error")
+    }
+}
+
+
+
